47d87b12bc5408a7145ee21e6edf1df4cecfa663
magicpalmtree <magicpalmtree.space@gmail.com>
2017-05-17 13:30:13 +0200

    Add dummy data for UX/UI testing purposes

--- client/app/components/finding/List.jsx
+++ client/app/components/finding/List.jsx
@@ -5,25 +5,33 @@ import {apiPrefix} from '../../App.jsx'
 // import FindingCreate from './FindingCreate.js';
 // import FindingEdit from './FindingEdit.js';
 
-import {Panel, ListGroup, ListGroupItem} from 'react-bootstrap';
+import {Tabs, Tab, Panel, ListGroup, ListGroupItem} from 'react-bootstrap';
 
 const panelStyle = {
     marginLeft: '10px',
     marginRight: '10px'
 };
 
-const itemStyle = {
-    margin: '10px 0'
-};
-
-
-
-const headers = [
+const headersStatic = [
     "Lokalita", "Kraj", "Okres", "Katastr", "Nadmořská výška",
     "Zkoumaná plocha", "Archeolog", "Rok výzkumu", "Instituce", "Koordináty XYZ",
     "Mapa", "Sonda", "Sektor", "Objekt", "Typ objektu",
     "Vrstva", "Hloubka(od do)", "Datace-archeologické období", "Datace-archeologická kultura",
     "Typ naleziště", "Reference", "Typ odběru", "Inventarizační číslo", "Poznámka"
+];
+
+const headersDynamBot = [
+    "Vzorek Botanický druh", "NISP (počet určených nálezů)", "Botanik",
+    "Rok určení", "Reference (archeobot.)","Stav zachování", "Poznámka","Objem"
+];
+
+const headersDynamZoo = [
+    "Číslo",
+    "Zoologický druh",
+    "NISP (počet určených nálezů)",
+    "Archeozoolog",
+    "Rok určení",
+    "Reference (archeozoo.)"
 ];
 
 export default class List extends React.Component {
@@ -34,7 +42,6 @@ export default class List extends React.Component {
         this.state = {
             findings: [],
             showEditModal: false,
-            showCreateModal: false
         }
     }
 
@@ -46,38 +53,81 @@ export default class List extends React.Component {
                 justifyContent: 'flex-start',
                 flexWrap: 'wrap'
             }}>
-                <Panel style={panelStyle} header="Panel heading">
-                    Archeobotanika
-                    <ListGroup fill>
-                        {
-                            headers.map((header) => {
-                                return <ListGroupItem key={header}>{header}</ListGroupItem>
-                            })
-                        }
+                <Panel style={panelStyle} header="Archeobotanika">
+                    <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
+                        <Tab eventKey={1} title="Static">
+                            <ListGroup fill>
+                                {
+                                    headersStatic.map((header) => {
+                                        return <ListGroupItem className="list-item" key={header}>{header}</ListGroupItem>
+                                    })
+                                }
 
-                    </ListGroup>
+                            </ListGroup>
+                        </Tab>
+                        <Tab eventKey={2} title="Dynamic">
+                            <ListGroup fill>
+                                {
+                                    headersDynamBot.map((header) => {
+                                        return <ListGroupItem className="list-item"
+                                                              key={header}>{header}</ListGroupItem>
+                                    })
+                                }
+                            </ListGroup>
+                        </Tab>
+                    </Tabs>
+
                 </Panel>
 
-                <Panel style={panelStyle} header="Panel heading">
-                    Archeobotanika
-                    <ListGroup fill>
-                        {
-                            headers.map((header) => {
-                                return <ListGroupItem key={header}>{header}</ListGroupItem>
-                            })
-                        }
-                    </ListGroup>
+                <Panel style={panelStyle} header="Archeobotanika">
+                    <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
+                        <Tab eventKey={1} title="Static">
+                            <ListGroup fill>
+                                {
+                                    headersStatic.map((header) => {
+                                        return <ListGroupItem className="list-item" key={header}>{header}</ListGroupItem>
+                                    })
+                                }
+
+                            </ListGroup>
+                        </Tab>
+                        <Tab eventKey={2} title="Dynamic">
+                            <ListGroup fill>
+                                {
+                                    headersDynamBot.map((header) => {
+                                        return <ListGroupItem className="list-item"
+                                                              key={header}>{header}</ListGroupItem>
+                                    })
+                                }
+                            </ListGroup>
+                        </Tab>
+                    </Tabs>
+
                 </Panel>
 
-                <Panel style={panelStyle} header="Panel heading">
-                    Archeozoologie
-                    <ListGroup fill>
-                        {
-                            headers.map((header) => {
-                                return <ListGroupItem key={header}>{header}</ListGroupItem>
-                            })
-                        }
-                    </ListGroup>
+                <Panel style={panelStyle} header="Archeozoologie">
+                    <Tabs defaultActiveKey={1} id="uncontrolled-tab-example">
+                        <Tab eventKey={1} title="Static">
+                            <ListGroup fill>
+                                {
+                                    headersStatic.map((header) => {
+                                        return <ListGroupItem className="list-item" key={header}>{header}</ListGroupItem>
+                                    })
+                                }
+
+                            </ListGroup>
+                        </Tab>
+                        <Tab eventKey={2} title="Dynamic">
+                            <ListGroup fill>
+                                {
+                                    headersDynamZoo.map((header) => {
+                                        return <ListGroupItem className="list-item"
+                                                              key={header}>{header}</ListGroupItem>
+                                    })
+                                }
+                            </ListGroup>
+                        </Tab>
+                    </Tabs>
                 </Panel>
             </div>
         );
@@ -102,14 +152,6 @@ export default class List extends React.Component {
 
     closeEditModal() {
         this.setState({ showEditModal: false });
-    }
-
-    openCreateModal() {
-        this.setState({ showCreateModal: true });
-    }
-
-    closeCreateModal() {
-        this.setState({ showCreateModal: false });
     }
 
     refreshList() {
