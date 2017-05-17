3c7c7fd157418f42f0968c5f403e22f6289cac51
magicpalmtree <magicpalmtree.space@gmail.com>
2017-05-17 21:12:40 +0200

    Separate schema definition into two types and pass it to Schema component

--- client/app/components/finding/Create.jsx
+++ client/app/components/finding/Create.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import schema from '../../../schema.json';
+import schemaBot from '../../../schemaBot.json';
+import schemaZoo from '../../../schemaZoo.json';
 import axios from 'axios';
 import {apiPrefix} from "../../App.jsx";
 
 import Form from "react-jsonschema-form";
 
+import {FormGroup, ControlLabel, FormControl} from 'react-bootstrap'
 
 // Perform a post request to save a formData
 const onSubmit = ({formData}) =>  axios.post(apiPrefix, formData)
@@ -15,9 +17,34 @@ const onSubmit = ({formData}) =>  axios.post(apiPrefix, formData)
 
 export default class Create extends React.Component {
 
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            // Default value
+            selectValue: "AB"
+        }
+    }
+
+    changeHandler(e) {
+        this.setState({
+            selectValue: e.target.value
+        });
+    }
+
     render() {
         return (
-            <Form schema={schema}/>
+            <div>
+                <FormGroup controlId="formControlsSelect">
+                    <ControlLabel>Select</ControlLabel>
+                    <FormControl componentClass="select" onChange={this.changeHandler.bind(this)} placeholder="select">
+                        <option value="AB">Archeobotanický záznam</option>
+                        <option value="AZ">Archeozoologický záznam</option>
+                    </FormControl>
+                </FormGroup>
+
+                <Form schema={this.state.selectValue === 'AB' ? schemaBot : schemaZoo}/>
+            </div>
         )
     }
 }
